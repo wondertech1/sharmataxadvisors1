b330b8d86b5e522509cf8c701cda723564f9c967
WonderTech <39814645+wondertech1@users.noreply.github.com>
2025-01-11 16:24:40 -0500

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,23 +1,13 @@
 //import * as React from 'react';
 import Box from '@mui/material/Box';
 import SocialMedia from "./SocialMedia";
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
+
 
 
 import Grid from '@mui/material/Grid2';
 
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    ...theme.applyStyles('dark', {
-      backgroundColor: '#1A2027',
-    }),
-  }));
+
 
 
 export default function BoxBasic() {
